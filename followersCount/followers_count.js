6c98fae983396cee31d7f27ec49526d72316ab54
Rodney Johnson <rodney@foxbadger.com>
2025-01-26 18:50:08 -0500

    Added Followers Count

--- followersCount/followers_count.js
+++ followersCount/followers_count.js
@@ -15,6 +15,23 @@ function checkCountValue() {
         alert("Your Instagram post gained 10 followers! Congratulations!");
     } else if (count === 20) {
         alert("Your Instagram post gained 20 followers! Keep it up!");
+    } else if (count === 0) {
+        alert("The counter has been reset");
     }
 }
 
+/*
+In this task you need to create a reset button which will reset the followers count code. For this you need to include:
+
+One button to represent Reset Count in followers_count.html file.
+
+Then create one function in followers_count.js file to reset count to 0.
+
+Also use alert popup box method to show alert message which will say that the Followers count has been reset.
+*/
+
+function resetCount() {
+    count = 0;
+    document.getElementById('countDisplay').innerHTML=count; // Set the counter to 0
+    checkCountValue();
+}
